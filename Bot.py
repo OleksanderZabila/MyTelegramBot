244e97eeef9f8b01cd127c4e85fb77998a410671
Oleksander Zabila <ukzabila@gmail.com>
2023-07-22 00:42:02 +0300

    Start work TelegramBot

--- Bot.py
+++ Bot.py
@@ -1,1 +1,8 @@
-print("hi")
+import telebot
+import config 
+bot = telebot.Telebot(config.TOKEN)
+@bot.message_handler(contetn_types=['text'])
+ def text_bot(message):
+     bot.send_message(message.chat.id, message.text)
+
+bot.polling(none_stop=True)
